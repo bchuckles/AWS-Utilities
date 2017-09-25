e78237e51d50125498fdbd26dde2c2aae1041046
Joe Tennant <joe.tennant@7signal.com>
2017-09-25 11:11:40 -0400

    Updated python script a bit to work with 7signal MQ installation

--- rabbitmq-to-cloudwatch.py
+++ rabbitmq-to-cloudwatch.py
@@ -8,37 +8,31 @@ from time import sleep
 
 def get_queue_depths(host, username, password, vhost):
     cl = Client(host, username, password)
-    if not cl.is_alive():
-        raise Exception("Failed to connect to rabbitmq")
     depths = {}
     queues = [q['name'] for q in cl.get_queues(vhost=vhost)]
     for queue in queues:
-        if queue == "aliveness-test": #pyrabbit
-            continue
-        elif queue.endswith('.pidbox') or queue.startswith('celeryev.'): #celery
-            continue
         depths[queue] = cl.get_queue_depth(vhost, queue)
     return depths
 
-
-def publish_queue_depth_to_cloudwatch(cwc, queue_name, depth, namespace):
+def publish_queue_depth_to_cloudwatch(cwc, queue_name, depth, namespace, envrionment):
     print("Putting metric namespace=%s name=%s unit=Count value=%i" % 
         (namespace, queue_name, depth))
     cwc.put_metric_data(namespace=namespace,
         name=queue_name,
+        dimensions={"envrionment": envrionment},
         unit="Count",
         value=depth)
 
 
-def publish_depths_to_cloudwatch(depths, namespace):
+def publish_depths_to_cloudwatch(depths, namespace, envrionment):
     cwc = CloudWatchConnection()
     for queue in depths:
-        publish_queue_depth_to_cloudwatch(cwc, queue, depths[queue], namespace)
+        publish_queue_depth_to_cloudwatch(cwc, queue, depths[queue], namespace, envrionment)
 
 
-def get_queue_depths_and_publish_to_cloudwatch(host, username, password, vhost, namespace):
+def get_queue_depths_and_publish_to_cloudwatch(host, username, password, vhost, namespace, envrionment):
     depths = get_queue_depths(host, username, password, vhost)
-    publish_depths_to_cloudwatch(depths, namespace)
+    publish_depths_to_cloudwatch(depths, namespace, envrionment)
 
 if __name__ == "__main__":
     while True:
@@ -47,5 +41,6 @@ if __name__ == "__main__":
             os.environ.get("rabbitmq_management_user"),
             os.environ.get("rabbitmq_management_password"),
             "/",
-            "rabbitmq_depth")
-        sleep(60 * 5)
+            "rabbitmq_depth",
+            os.environ.get("rabbitmq_management_environment"))
+        sleep(60 * 1)
